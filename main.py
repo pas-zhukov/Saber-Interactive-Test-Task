88e26d1c5b1f911ba3a8d8a4ee8d2c3879f8ad45
Pavel <pas-zhukov@yandex.ru>
2023-05-02 00:21:44 +0300

    Initial Commit.

---
+++ main.py
@@ -0,0 +1,72 @@
+import click
+from models import load_data
+
+# Путь к файлам tasks.yaml и builds.yaml
+# Используем dict для дальнейшего удобства создания команд
+PATH_TO_FILES = {
+    'tasks': 'tasks.yaml',
+    'builds': 'builds.yaml'
+}
+# Загружаем данные из файлов в глобальные переменные
+TASKS, BUILDS = load_data(*PATH_TO_FILES.values())
+
+
+@click.group()
+def commands():
+    pass
+
+
+@click.command('list')
+@click.argument('entity',
+                type=click.Choice(PATH_TO_FILES.keys()),
+                required=1,  # обязательный аргумент
+                )
+def list(entity: str):
+    """
+    Prints the list of tasks or builds names loaded.
+
+    """
+    if entity == 'tasks':
+        click.echo('List of available tasks:')
+        for task in TASKS:
+            click.echo(f'* {task}')
+    else:
+        click.echo('List of available builds:')
+        for build in BUILDS:
+            click.echo(f'* {build}')
+
+
+@click.command('get')
+@click.argument('entity',
+                type=click.Choice(['task', 'build']),
+                required=1,
+                )
+@click.argument('name',
+                type=str,
+                required=1,
+                )
+def get(entity: str, name: str):
+    """
+    Prints detailed info about specific task or build.
+
+    NAME: Specify the name of task/build.
+    """
+    if entity == 'task':
+        tasks_names = [x.name for x in TASKS]
+        try:
+            click.echo(TASKS[tasks_names.index(name)].info())
+        except ValueError:
+            click.echo(f'{entity.title()} named "{name}" does not exist!')
+    else:
+        builds_names = [x.name for x in BUILDS]
+        try:
+            click.echo(BUILDS[builds_names.index(name)].info())
+        except ValueError:
+            click.echo(f'{entity.title()} named "{name}" does not exist!')
+
+
+commands.add_command(list)
+commands.add_command(get)
+
+if __name__ == '__main__':
+    commands()
---
+++ models.py
@@ -0,0 +1,79 @@
+import yaml
+
+
+class Task:
+    def __init__(self, name: str, dependencies: list[str]):
+        self.name = name
+        self.dependencies = dependencies
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}'{(', ' + str(self.dependencies)) if len(self.dependencies) > 0 else ''}) "
+
+    def __len__(self):
+        return len(self.dependencies)
+
+    def __getitem__(self, item):
+        return self.dependencies[item]
+
+    def info(self):
+        info = f"""
+        Task info:
+        * name: {self.name}
+        * dependencies: """
+        info += ', '.join(self.dependencies)
+        return info
+
+
+class Build:
+    def __init__(self, name: str, tasks: list[Task]):
+        self.name = name
+        self.tasks = tasks
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}'{(', ' + str(self.tasks)) if len(self.tasks) > 0 else ''}"
+
+    def __len__(self):
+        return len(self.tasks)
+
+    def __getitem__(self, item):
+        return self.tasks[item]
+
+    def info(self):
+        info = f"""
+        Task info:
+        * name: {self.name}
+        * tasks: """
+        tasks = [[x for x in y.dependencies] + [y.name] for y in self.tasks]
+        tasks = sum(tasks, [])
+        info += ', '.join(tasks)
+        return info
+
+
+def load_data(path_to_tasks: str = 'tasks.yaml', path_to_builds: str = 'builds.yaml') -> tuple[list[Task], list[Build]]:
+    tasks = []
+    builds = []
+    # Загружаем все tasks
+    with open(path_to_tasks, 'r') as f:
+        data = yaml.load(f, yaml.Loader)
+        for task in data['tasks']:
+            tasks.append(Task(task['name'], task['dependencies']))
+
+    # Загружаем все builds, подключаем к ним уже созданные объекты Task
+    with open(path_to_builds, 'r') as f:
+        data = yaml.load(f, yaml.Loader)
+        for build in data['builds']:
+            task_names = build['tasks']
+            build_tasks = []
+            for task_name in task_names:
+                for i, task in enumerate(tasks):
+                    if task.name == task_name:
+                        build_tasks.append(tasks[i])
+            builds.append(Build(build['name'], build_tasks))
+
+    return tasks, builds
